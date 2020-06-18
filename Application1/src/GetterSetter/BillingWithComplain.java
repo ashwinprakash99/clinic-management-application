7597afba1b56776e6cbae362b4f7a028864e8082
kushkkumar <ankushnkumar1999@gmail.com>
2020-06-18 15:51:15 +0530

    billing basic

---
+++ Application1/src/GetterSetter/BillingWithComplain.java
@@ -0,0 +1,47 @@
+package GetterSetter;
+
+public class BillingWithComplain {
+    Long id;
+    String MedName;
+    Integer quantity;
+    Double price;
+
+    public BillingWithComplain(Long id, String medName, Integer quantity, Double price) {
+        this.id = id;
+        MedName = medName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMedName() {
+        return MedName;
+    }
+
+    public void setMedName(String medName) {
+        MedName = medName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
--- Application1/src/controller/BillingWithComplain.java
+++ Application1/src/controller/BillingWithComplain.java
@@ -1,8 +1,11 @@
 package controller;
 import dbConnector.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class BillingWithComplain {
 
@@ -10,19 +13,19 @@ public class BillingWithComplain {
     private TextField complaintId;
 
     @FXML
-    private TableView<?> tableView;
+    private TableView<GetterSetter.BillingWithComplain> tableView;
 
     @FXML
-    private TableColumn<?, ?> tabId;
+    private TableColumn<GetterSetter.BillingWithComplain, Long> tabId;
 
     @FXML
-    private TableColumn<?, ?> tabMed;
+    private TableColumn<GetterSetter.BillingWithComplain, String> tabMed;
 
     @FXML
-    private TableColumn<?, ?> tabQntty;
+    private TableColumn<GetterSetter.BillingWithComplain, Integer> tabQntty;
 
     @FXML
-    private TableColumn<?, ?> tabPrice;
+    private TableColumn<GetterSetter.BillingWithComplain, Double> tabPrice;
 
     @FXML
     private TextField cusultaion;
@@ -31,9 +34,29 @@ public class BillingWithComplain {
     private TextField extra;
 
     @FXML
-    void checkComplaint(ActionEvent event) {
+    void checkComplaint(ActionEvent event)  {
+        try {
+            CompleteMedicinePrescription[] cp=CompleteMedicinePrescription.getCompleteMedicineData(Long.parseLong(complaintId.getText()));
 
-        
+            ObservableList<GetterSetter.BillingWithComplain> list = FXCollections.observableArrayList();
+
+            tabId.setCellValueFactory(new PropertyValueFactory<>("id"));
+            tabMed.setCellValueFactory(new PropertyValueFactory<>("MedName"));
+            tabQntty.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+            tabPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+            for(int i=0;i<cp.length;i++){
+                list.add(new GetterSetter.BillingWithComplain((long) (i+1),cp[i].getMedicine().getMedicineName(),cp[i].getMedicinePrescription().getQuantity(),cp[i].getMedicine().getPrice()));
+                tableView.setItems(list);
+            }
+
+            
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
 
 
     }
