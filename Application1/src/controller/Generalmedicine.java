6ffc77c0f9dcfcb672ab2b625a5e1b1db38087f2
kushkkumar <ankushnkumar1999@gmail.com>
2020-06-20 16:30:19 +0530

    modified general Medicine fxml Page

--- Application1/src/controller/Generalmedicine.java
+++ Application1/src/controller/Generalmedicine.java
@@ -1,5 +1,35 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Generalmedicine {
+    @FXML
+    private TextField patientName;
+
+    @FXML
+    private TableColumn<?, ?> select;
+
+    @FXML
+    private TableColumn<?, ?> medName;
+
+    @FXML
+    private TableColumn<?, ?> quantity;
+
+    @FXML
+    private TableColumn<?, ?> cost;
+
+    @FXML
+    void generalMedicineClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void patientSubmitClick(ActionEvent event) {
+
+    }
+
+
 
 }
