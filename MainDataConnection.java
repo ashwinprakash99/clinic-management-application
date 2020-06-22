cc7f522375af097a566c3c5f1d6e70a0b5382f49
ashwinprakash99 <ashwinprakash99@gmail.com>
2020-06-22 13:08:26 +0530

    Added code to read credentials from a file

--- MainDataConnection.java
+++ MainDataConnection.java
@@ -1,14 +1,24 @@
 import java.sql.*;
+import java.io.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
 public class MainDataConnection {
     static public Connection connection = null;
+    
+    public static void init() throws Exception {
+        JSONObject jsonObject = (JSONObject) (new JSONParser().parse(new FileReader("connection.json")));
+        String host = (String) jsonObject.get("host");
+        String port = (String) jsonObject.get("port");
+        String username = (String) jsonObject.get("username");
+        String password = (String) jsonObject.get("password");
+        String database = (String) jsonObject.get("database");
+        String connectionString = "jdbc:mysql://" + host + ":" + port + "/" + database;
+        connection = DriverManager.getConnection(connectionString, username, password);
+        System.out.println("Connection Established Successfully...");
+    }
 
-    static {
-        try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clinic_management_application", "root", "root");
-            System.out.println("Connection successfull Static connection...");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+    public static void main(String[] args) throws Exception {
+        MainDataConnection.init();
     }
 }
